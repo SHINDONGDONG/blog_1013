9bbd063c4f931f53207832a9a1e9f4d13ff7b799
cos <homerjay@naver.com>
2020-10-13 23:31:25 +0900

    초기환경

---
+++ src/main/java/com/cos/blog_1013/Blog1013Application.java
@@ -0,0 +1,13 @@
+package com.cos.blog_1013;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blog1013Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Blog1013Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cos/blog_1013/Blog1013ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.blog_1013;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Blog1013ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
